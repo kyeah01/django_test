ff6657b5d08812613e83e4b145c4adb21635983b
kyeah01 <kyeah01@gmail.com>
2019-04-23 04:43:26 +0000

    04 | index & delete & update view test

--- boards/tests.py
+++ boards/tests.py
@@ -1,6 +1,7 @@
 # from django.test import TestCase
 from test_plus.test import TestCase
 from django.conf import settings
+from django.urls import reverse
 from .models import Board
 from .forms import BoardForm
 
@@ -41,14 +42,14 @@ class BoardModelTest(TestCase):
             'title': '제목', 
         }
         self.assertEqual(BoardForm(data).is_valid(), False)
-        
+    
         
 class BoardViewTest(TestCase):
     
     # 반복되는 make user를 한번에 만들어주기 위해서 정의
     # 공통적인 given 상황을 구성하기에 유용하다.
     def setUp(self):
-        user = self.make_user(username='test', password='1234567!')
+        self.user = self.make_user(username='test', password='1234567!')
         
     
     # create test의 포인트는 form을 제대로 주는가이다.
@@ -85,7 +86,8 @@ class BoardViewTest(TestCase):
 
             # then
             self.post('boards:create', data=data)
-            # self.assertEqual(response.status_code, 200)
+            self.response_302()
+            # self.assertEqual(response.status_code, 302)
             
     def test_04_board_create_without_content(self):
         
@@ -98,3 +100,71 @@ class BoardViewTest(TestCase):
         with self.login(username='test', password='1234567!'):
             response = self.post('boards:create', data=data)
             self.assertContains(response, '')
+    
+    # 디테일 페이지가 제대로 출력되는지 확인
+    def test_05_detail_contains(self):
+        # given
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        # when
+        self.get_check_200('boards:detail', board_pk=board.pk)
+        
+        # then
+        self.assertResponseContains(board.title, html=False)
+        self.assertResponseContains(board.content, html=False)
+        
+    def test_06_detail_templates(self):
+        # given
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        
+        # when
+        response = self.get_check_200('boards:detail', board_pk=board.pk)
+        
+        # then
+        self.assertTemplateUsed(response, 'boards/detail.html')
+        
+    def test_07_get_index(self):
+        # given when then
+        self.get_check_200('boards:index')
+        
+    def test_08_index_template(self):
+        response = self.get_check_200('boards:index')
+        self.assertTemplateUsed(response, 'boards/index.html')
+        
+    def test_09_index_queryset(self):
+        # given | 글 두개 작성
+        Board.objects.create(title='제목', content='내용', user=self.user)
+        Board.objects.create(title='제목', content='내용', user=self.user)
+        boards = Board.objects.order_by('-pk')
+        
+        # when
+        response = self.get_check_200('boards:index')
+        
+        # then
+        self.assertQuerysetEqual(response.context['boards'], map(repr, boards))
+        
+    def test_10_delete(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='1234567!'):
+            self.get_check_200('boards:delete', board_pk=board.pk)
+        
+    def test_11_delete_post(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='1234567!'):
+            self.post('boards:delete', board_pk=board.pk)
+            self.assertEqual(Board.objects.count(), 0)
+            
+    def test_12_delete_redirect(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='1234567!'):
+            response = self.post('boards:delete', board_pk=board.pk)
+            self.assertRedirects(response, reverse('boards:index'))
+            
+    def test_13_get_update(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='1234567!'):
+            response = self.get_check_200('boards:update', board.pk)
+            self.assertEqual(response.context['form'].instance.pk, board.pk)
+            self.post('boards:update', board_pk=board.pk)
+            
+    def test_14_get_update_login_required(self):
+        self.assertLoginRequired('boards:update', board_pk=1)
